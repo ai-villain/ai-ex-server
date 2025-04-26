e9c3aec63d1faf62505f95aeff5de328993e0f5c
yunsseong <aster20@hanyang.ac.kr>
2025-04-26 16:03:17 +0900

    chore: 스프링 프로젝트 구성

---
+++ src/main/java/com/yunsseong/ai_ex_server/AiExServerApplication.java
@@ -0,0 +1,13 @@
+package com.yunsseong.ai_ex_server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AiExServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AiExServerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/yunsseong/ai_ex_server/AiExServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yunsseong.ai_ex_server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AiExServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
