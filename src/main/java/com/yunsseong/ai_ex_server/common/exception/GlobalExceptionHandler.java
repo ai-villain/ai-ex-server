b1ce17155948cbfad229acc4cb1a047e4bdd9722
yunsseong <aster20@hanyang.ac.kr>
2025-05-01 15:26:08 +0900

    feat: 자바 빈 검증 예외 전역 예외 핸들러 구현

--- src/main/java/com/yunsseong/ai_ex_server/common/exception/GlobalExceptionHandler.java
+++ src/main/java/com/yunsseong/ai_ex_server/common/exception/GlobalExceptionHandler.java
@@ -2,11 +2,15 @@ package com.yunsseong.ai_ex_server.common.exception;
 
 import com.yunsseong.ai_ex_server.common.exception.dto.response.ErrorResponse;
 import com.yunsseong.ai_ex_server.common.exception.error_code.ErrorCode;
+import org.springframework.context.support.DefaultMessageSourceResolvable;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 import java.time.LocalDateTime;
+import java.util.stream.Collectors;
 
 @RestControllerAdvice
 public class GlobalExceptionHandler {
@@ -20,5 +24,22 @@ public class GlobalExceptionHandler {
                 .timestamp(LocalDateTime.now())
                 .build();
         return ResponseEntity.status(errorCode.getHttpStatus()).body(response);
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<ErrorResponse> handleValidationException(MethodArgumentNotValidException ex) {
+        String message = ex.getBindingResult()
+                .getFieldErrors()
+                .stream()
+                .map(DefaultMessageSourceResolvable::getDefaultMessage)
+                .collect(Collectors.joining(", "));
+        return ResponseEntity.badRequest()
+                .body(
+                        new ErrorResponse(
+                                HttpStatus.BAD_REQUEST.value(),
+                                HttpStatus.BAD_REQUEST.getReasonPhrase(),
+                                message,
+                                LocalDateTime.now())
+                );
     }
 }
