a38184b0b05c90d6cbad51e1f0ff44d7cec5d5a6
yunsseong <aster20@hanyang.ac.kr>
2025-05-03 00:01:20 +0900

    feat: 멤버 생성 요청 DTO 빌더 추가

--- src/main/java/com/yunsseong/ai_ex_server/member/application/dto/CreateMemberRequest.java
+++ src/main/java/com/yunsseong/ai_ex_server/member/application/dto/CreateMemberRequest.java
@@ -1,4 +1,7 @@
 package com.yunsseong.ai_ex_server.member.application.dto;
 
+import lombok.Builder;
+
+@Builder
 public record CreateMemberRequest(String nickname, String email, String password) {
 }
