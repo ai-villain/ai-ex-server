7edd9c21415999e6210a17e3aa36018077ae8bb3
yunsseong <aster20@hanyang.ac.kr>
2025-05-03 00:05:26 +0900

    feat: 멤버 객체 class로 변경 및 UserDetails 상속

--- src/main/java/com/yunsseong/ai_ex_server/member/domain/Member.java
+++ src/main/java/com/yunsseong/ai_ex_server/member/domain/Member.java
@@ -2,7 +2,53 @@ package com.yunsseong.ai_ex_server.member.domain;
 
 import jakarta.validation.constraints.NotNull;
 import lombok.Builder;
+import lombok.Getter;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
+import java.util.List;
 
 @Builder
-public record Member(Long userId, @NotNull Nickname nickname, @NotNull Email email, @NotNull Password password) {
+@Getter
+public class Member implements UserDetails {
+    private Long memberId;
+    private @NotNull Nickname nickname;
+    private @NotNull Email email;
+    private @NotNull Password password;
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return List.of();
+    }
+
+    @Override
+    public String getPassword() {
+        return password.password();
+    }
+
+    @Override
+    public String getUsername() {
+        return email.email();
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
 }
