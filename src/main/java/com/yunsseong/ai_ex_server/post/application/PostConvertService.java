356de697cfac12f89dce7fe9134c5c94bdc6d610
yunsseong <aster20@hanyang.ac.kr>
2025-05-13 11:40:18 +0900

    refactor: 클래스 타입 변경, 도메인 객체 변경으로 인한 컨버터 변경

--- src/main/java/com/yunsseong/ai_ex_server/post/application/PostConvertService.java
+++ src/main/java/com/yunsseong/ai_ex_server/post/application/PostConvertService.java
@@ -1,11 +1,8 @@
 package com.yunsseong.ai_ex_server.post.application;
 
-import com.yunsseong.ai_ex_server.member.domain.Member;
 import com.yunsseong.ai_ex_server.post.application.dto.PostResponse;
 import com.yunsseong.ai_ex_server.post.application.dto.UpdatePostRequest;
-import com.yunsseong.ai_ex_server.post.domain.Content;
 import com.yunsseong.ai_ex_server.post.domain.Post;
-import com.yunsseong.ai_ex_server.post.domain.Title;
 import org.springframework.stereotype.Service;
 
 import java.time.LocalDateTime;
@@ -15,21 +12,21 @@ public class PostConvertService {
 
     public Post createPostFromUpdateRequest(UpdatePostRequest request) {
         return Post.builder()
-                .postId(request.postId())
-                .title(new Title(request.title()))
-                .content(new Content(request.content()))
+                .id(request.postId())
+                .title(request.title())
+                .content(request.content())
                 .updatedAt(LocalDateTime.now())
                 .build();
     }
 
-    public PostResponse createPostResponse(Post post, Member member) {
+    public PostResponse createPostResponse(Post post) {
         return PostResponse.builder()
-                .postId(post.postId())
-                .nickname(member.getNickname().nickname())
-                .title(post.title().title())
-                .content(post.content().content())
-                .createdAt(post.createdAt().toString())
-                .updatedAt(post.updatedAt().toString())
+                .postId(post.getId())
+                .nickname(post.getMember().getNickname())
+                .title(post.getTitle())
+                .content(post.getContent())
+                .createdAt(post.getCreatedAt().toString())
+                .updatedAt(post.getUpdatedAt().toString())
                 .build();
     }
 }
