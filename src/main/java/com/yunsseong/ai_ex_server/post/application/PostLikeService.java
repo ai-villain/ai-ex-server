fc3c197bbaa8bce2a65ad93c536fc746c248adeb
yunsseong <aster20@hanyang.ac.kr>
2025-05-20 11:56:11 +0900

    feat: 게시물 좋아요 기능 구현

---
+++ src/main/java/com/yunsseong/ai_ex_server/post/application/PostLikeService.java
@@ -0,0 +1,44 @@
+package com.yunsseong.ai_ex_server.post.application;
+
+import com.yunsseong.ai_ex_server.member.application.MemberService;
+import com.yunsseong.ai_ex_server.member.domain.Member;
+import com.yunsseong.ai_ex_server.post.domain.Post;
+import com.yunsseong.ai_ex_server.post.domain.PostLike;
+import com.yunsseong.ai_ex_server.post.infrastructure.PostLikeRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+@Service
+@RequiredArgsConstructor
+public class PostLikeService {
+
+    private final PostLikeRepository postLikeRepository;
+    private final PostService postService;
+    private final MemberService memberService;
+
+    public boolean likePost(Long postId, Long memberId) {
+        Post foundPost = postService.findById(postId);
+        Member foundMember = memberService.findById(memberId);
+        Optional<PostLike> foundLike = postLikeRepository.findByPostAndMember(foundPost, foundMember);
+
+        if (foundLike.isPresent()) {
+            postLikeRepository.delete(foundLike.get());
+            return false;
+        }
+
+        postLikeRepository.save(
+                PostLike.builder()
+                .post(foundPost)
+                .member(foundMember)
+                .build()
+        );
+        return true;
+    }
+
+    public Long likeCount(Long postId) {
+        Post foundPost = postService.findById(postId);
+        return postLikeRepository.findByPost(foundPost).stream().count();
+    }
+}
--- src/main/java/com/yunsseong/ai_ex_server/post/presentation/PostController.java
+++ src/main/java/com/yunsseong/ai_ex_server/post/presentation/PostController.java
@@ -2,16 +2,16 @@ package com.yunsseong.ai_ex_server.post.presentation;
 
 import com.yunsseong.ai_ex_server.common.dto.ApiResponse;
 import com.yunsseong.ai_ex_server.common.dto.ApiResponseFactory;
-import com.yunsseong.ai_ex_server.member.application.MemberService;
-import com.yunsseong.ai_ex_server.member.domain.Member;
+import com.yunsseong.ai_ex_server.member.domain.CustomUserDetails;
+import com.yunsseong.ai_ex_server.post.application.PostLikeService;
 import com.yunsseong.ai_ex_server.post.application.PostService;
-import com.yunsseong.ai_ex_server.post.application.dto.CreatePostRequest;
-import com.yunsseong.ai_ex_server.post.application.dto.PostResponse;
-import com.yunsseong.ai_ex_server.post.presentation.dto.WritePostRequest;
+import com.yunsseong.ai_ex_server.post.dto.LikeCountResponse;
+import com.yunsseong.ai_ex_server.post.dto.LikeResponse;
+import com.yunsseong.ai_ex_server.post.dto.PostResponse;
+import com.yunsseong.ai_ex_server.post.dto.CreatePostRequest;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
-import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -22,16 +22,31 @@ import java.util.List;
 public class PostController {
 
     private final PostService postService;
-    private final MemberService memberService;
+    private final PostLikeService postLikeService;
 
     @GetMapping
     public ResponseEntity<ApiResponse<List<PostResponse>>> getPosts() {
         return ApiResponseFactory.success(postService.findAll());
     }
 
+    @GetMapping("/{postId}")
+    public ResponseEntity<ApiResponse<PostResponse>> getPost(@PathVariable Long postId) {
+        return ApiResponseFactory.success(postService.getPost(postId));
+    }
+
     @PostMapping
     public ResponseEntity<ApiResponse<Void>> writePost(@RequestBody CreatePostRequest request, @AuthenticationPrincipal CustomUserDetails userDetails) {
         postService.createPost(request, userDetails.getMemberId());
         return ApiResponseFactory.success();
+    }
+
+    @PostMapping("/{postId}/like")
+    public ResponseEntity<ApiResponse<LikeResponse>> likePost(@PathVariable Long postId, @AuthenticationPrincipal CustomUserDetails userDetails) {
+        return ApiResponseFactory.success(new LikeResponse(postLikeService.likePost(postId, userDetails.getMemberId())));
+    }
+
+    @GetMapping("/{postId}/like")
+    public ResponseEntity<ApiResponse<LikeCountResponse>> likeCount(@PathVariable Long postId) {
+        return ApiResponseFactory.success(new LikeCountResponse(postLikeService.likeCount(postId)));
     }
 }
