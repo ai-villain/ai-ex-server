a8006eaf77c1f9d1dc872da030052de8f1642369
yunsseong <aster20@hanyang.ac.kr>
2025-05-26 18:09:19 +0900

    fix: PostLike 조회 메서드명 변경

--- src/main/java/com/yunsseong/ai_ex_server/post/infrastructure/PostLikeRepository.java
+++ src/main/java/com/yunsseong/ai_ex_server/post/infrastructure/PostLikeRepository.java
@@ -1,14 +1,10 @@
 package com.yunsseong.ai_ex_server.post.infrastructure;
 
-import com.yunsseong.ai_ex_server.member.domain.Member;
-import com.yunsseong.ai_ex_server.post.domain.Post;
 import com.yunsseong.ai_ex_server.post.domain.PostLike;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import java.util.List;
 import java.util.Optional;
 
 public interface PostLikeRepository extends JpaRepository<PostLike, Long> {
-    Optional<PostLike> findByPostAndMember(Post post, Member member);
-    List<PostLike> findByPost(Post post);
+    Optional<PostLike> findByPostIdAndMemberId(Long postId, Long memberId);
 }
